81b6d9b54aff27db5d6bfc5d412fbfa2e3965d4a
Elric LESCOP <elriclescop31@gmail.com>
2024-12-08 09:21:10 +0100

    Day_06

--- Day_06/6.py
+++ Day_06/6.py
@@ -42,41 +42,59 @@ with open("input.txt", "r") as file:
         if liste[i].count("v") != 0:
             pos = [i, liste[i].find("v")]
         liste[i] = [liste[i][j] for j in range(len(liste[i]))]
-    original = copy_2d(liste)
     somme = 0
-    original_pos = pos.copy()
-    for i in range(len(liste)):
-        for j in range(len(liste[i])):
-            if [i, j] != original_pos:
-                liste[i][j] = "#"
-            condition = True
-            index = 0
-            while condition and index < 20000:
-                try:
-                    new_pos = [pos[0]+carac[ge(liste, pos)][0], pos[1]+carac[ge(liste, pos)][1]]
-                except:
-                    exit()
+    boolean = bool(int(input("First star (1) or Second star (2) : "))-1)
+    if boolean:
+        original = copy_2d(liste)
+        original_pos = pos.copy()
+        for i in range(len(liste)):
+            for j in range(len(liste[i])):
+                if [i, j] != original_pos:
 
-                if out_of_bounds(liste, new_pos):
-                    condition = False
-                    continue
-                if ge(liste, new_pos) == "#":
-                    if liste[pos[0]][pos[1]] == "^":
-                        liste[pos[0]][pos[1]] = ">"
-                    elif liste[pos[0]][pos[1]] == ">":
-                        liste[pos[0]][pos[1]] = "v"
-                    elif liste[pos[0]][pos[1]] == "v":
-                        liste[pos[0]][pos[1]] = "<"
-                    elif liste[pos[0]][pos[1]] == "<":
-                        liste[pos[0]][pos[1]] = "^"
-                else:
-                    se(liste, new_pos, ge(liste, pos))
-                    se(liste, pos, ".")
-                    pos = new_pos
-                index += 1
-            if index >= 20000:
-                somme += 1
-            liste = copy_2d(original)
-            pos = original_pos.copy()
-            print(f"{j+len(liste[0])*i}/{len(liste)*len(liste[0])}")
+                    liste[i][j] = "#"
+                condition = True
+                index = 0
+                while condition and index < 20000:
+                    new_pos = [pos[0]+carac[ge(liste, pos)][0], pos[1]+carac[ge(liste, pos)][1]]
+                    if out_of_bounds(liste, new_pos):
+                        condition = False
+                        continue
+                    if ge(liste, new_pos) == "#":
+                        turn_right(liste, pos)
+                    else:
+                        se(liste, new_pos, ge(liste, pos))
+                        se(liste, pos, ".")
+                        pos = new_pos
+                    index += 1
+                if index >= 20000:
+                    somme += 1
+                liste = copy_2d(original)
+                pos = original_pos.copy()
+    else:
+        x_list = copy_2d(liste)
+        condition = True
+        while condition:
+            x_list[pos[0]][pos[1]] = "X"
+            new_pos = [pos[0]+carac[ge(liste, pos)][0], pos[1]+carac[ge(liste, pos)][1]]
+            if out_of_bounds(liste, new_pos):
+                condition = False
+                continue
+            if ge(liste, new_pos) == "#":
+                if liste[pos[0]][pos[1]] == "^":
+                    liste[pos[0]][pos[1]] = ">"
+                elif liste[pos[0]][pos[1]] == ">":
+                    liste[pos[0]][pos[1]] = "v"
+                elif liste[pos[0]][pos[1]] == "v":
+                    liste[pos[0]][pos[1]] = "<"
+                elif liste[pos[0]][pos[1]] == "<":
+                    liste[pos[0]][pos[1]] = "^"
+            else:
+                se(liste, new_pos, ge(liste, pos))
+                se(liste, pos, ".")
+                pos = new_pos
+        for i in range(len(x_list)):
+            for j in range(len(x_list[i])):
+                if x_list[i][j] == "X":
+                    somme += 1
+
 print(somme)
