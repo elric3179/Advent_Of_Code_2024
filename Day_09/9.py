29897a14b82b7df7dca2db4c5b4a929ad7e9e448
Elric LESCOP <elriclescop31@gmail.com>
2024-12-10 09:49:31 +0100

    Day_09

--- Day_09/9.py
+++ Day_09/9.py
@@ -2,7 +2,6 @@ def is_sorted(liste):
     l = len(liste) -1
     for i in range(len(liste)-1):
         if liste[l - i - 1] == -1 and liste[l - i] != -1:
-            print(l-i-1)
             return False        
     return True
 
@@ -11,9 +10,9 @@ def get_last(liste, i_2):
         if liste[len(liste)-1-i] != -1:
             return len(liste)-1-i
 
-def get_first(liste, length):
-    for i in range(len(liste)-length):
-        if [liste[i+j] for j in range(length)] == [-1]*length:
+def get_first1(liste, i_1):
+    for i in range(i_1, len(liste)):
+        if liste[i] == -1:
             return i
     return len(liste)
 
@@ -38,39 +37,64 @@ def find_last_value(liste, last_value):
     for i in range(len(liste)):
         if liste[i][0] == last_value:
             return i
+
 with open("input.txt", "r") as file:
     string = file.read()
     liste = [string[i] for i in range(len(string))]
     condition = 0
     string = []
+    boolean = bool(int(input("First star (1) or Second star (2) : "))-1)
     for i in range(len(liste)):
         if condition % 2 == 0:
-            string.append([condition//2,int(liste[i])])
-        else:
-            string.append([-1,int(liste[i])])
-        condition += 1
-    last_value = condition//2
-    liste = string
-    condition = True
-    while last_value >= 0:
-        last_index = find_last_value(liste, last_value)
-        first_index = find_first(liste, liste[last_index][1])
-        if first_index == None:
-            last_value -= 1
-            continue
-        if first_index < last_index:
-            if liste[first_index][1] == liste[last_index][1]:
-                temp = liste[first_index]
-                liste[first_index] = liste[last_index]
-                liste[last_index] = temp
+            if boolean:
+                string.append([condition//2, int(liste[i])])
             else:
-                temp = liste[last_index]
-                liste[last_index] = [-1, temp[1]]
-                liste[first_index] = [-1, liste[first_index][1]-temp[1]]
-                liste.insert(first_index, [last_value, temp[1]])
-        last_value -= 1
-new_liste = []
-for index, value in enumerate(liste):
-    for j in range(value[1]):
-        new_liste.append(value[0])
-print(check_sum(new_liste))
+                for j in range(int(liste[i])):
+                    string.append(condition//2)
+
+        else:
+            if boolean:
+                string.append([-1, int(liste[i])])
+            else:
+                for j in range(int(liste[i])):
+                    string.append(-1)
+        condition += 1
+
+    liste = string
+
+    if not boolean:
+        condition = True
+        i_1 = 0
+        i_2 = len(liste)
+        while condition:
+            i_1 = get_first1(liste, i_1)
+            i_2 = get_last(liste, i_2)
+            if i_1 < i_2:   
+                swap(liste, i_1, i_2)
+            else:
+                condition = False
+    else:
+        last_value = condition//2
+        while last_value >= 0:
+            last_index = find_last_value(liste, last_value)
+            first_index = find_first(liste, liste[last_index][1])
+            if first_index == None:
+                last_value -= 1
+                continue
+            if first_index < last_index:
+                if liste[first_index][1] == liste[last_index][1]:
+                    temp = liste[first_index]
+                    liste[first_index] = liste[last_index]
+                    liste[last_index] = temp
+                else:
+                    temp = liste[last_index]
+                    liste[last_index] = [-1, temp[1]]
+                    liste[first_index] = [-1, liste[first_index][1]-temp[1]]
+                    liste.insert(first_index, [last_value, temp[1]])
+            last_value -= 1
+        new_liste = []
+        for index, value in enumerate(liste):
+            for j in range(value[1]):
+                new_liste.append(value[0])
+        liste = new_liste.copy()
+    print(check_sum(liste))
